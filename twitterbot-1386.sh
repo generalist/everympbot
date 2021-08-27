12b80675a948c17accd0cb2c276c47d03d092c9b
Andrew Gray <github@generalist.org.uk>
2021-08-27 18:11:41 +0100

    Create twitterbot-1386.sh

---
+++ twitterbot-1386.sh
@@ -0,0 +1,24 @@
+# this is the overall script to make a tweet with the twitterbot
+# version 1.2
+# uses assembly.py and tweeting.py
+# 1386 data only
+
+# set working directory - change as needed
+
+cd ~/scripts/everymp
+
+# clean up old data to prevent retweeting if the query fails
+
+rm nexttweet.txt
+rm nextimage.txt
+rm twitterimage.*
+
+# first generate a new link
+
+python3 assembly-1386.py
+
+# then run the tweet script
+
+python3 tweeting.py
+
+# and you're done!
